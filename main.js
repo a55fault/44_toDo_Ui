2df808a55dcea5843ee7e933d89f706696731b13
Anusai <prorock7@ya.ru>
2022-02-24 20:04:51 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+/* alert ("start") */
+let defaultValueHight = "Добавить важных дел";
+let defaultValueLow = "Добавить дел";
+let emptyString = ""
+
+import { plusHighButtons } from "./veiw.js";
+import { plusLowButtons } from "./veiw.js";
+import { exButtons } from "./veiw.js";
+
+
+for (let elem of plusHighButtons) {
+    elem.addEventListener("submit", plusHighTask);
+}
+for (let elem of plusLowButtons) {
+    elem.addEventListener("submit", plusLowTask);
+}
+for (let elem of exButtons) {
+    elem.addEventListener("click", deleteTask);
+}
+
+
+function plusHighTask() {
+    let form = parentHighForm.cloneNode(true);
+    form.querySelector(".input.text").placeholder = parentHighForm.querySelector(".input.text").value;
+    parentHighForm.querySelector(".input.text").value = emptyString;
+    parentHighForm.querySelector(".input.text").placeholder = defaultValueHight;
+    form.querySelector(".button.high.plus").remove();
+    form.querySelector(".input.text").insertAdjacentHTML("afterend", `<button class="button ex" type="submit">x</button>`);
+    form.querySelector(".button.ex").addEventListener("click", deleteTask);
+    parentHighForm.after(form);
+}
+
+function plusLowTask() {
+    let form = parentLowForm.cloneNode(true);
+    form.querySelector(".input.text").placeholder = parentLowForm.querySelector(".input.text").value;
+    parentLowForm.querySelector(".input.text").value = emptyString;
+    parentLowForm.querySelector(".input.text").placeholder = defaultValueLow;
+    form.querySelector(".button.low.plus").remove();
+    form.querySelector(".input.text").insertAdjacentHTML("afterend", `<button class="button ex" type="submit">x</button>`);
+    form.querySelector(".button.ex").addEventListener("click", deleteTask);
+    parentLowForm.after(form);
+}
+
+
+function deleteTask () {   
+this.parentNode.remove();
+}
---
+++ veiw.js
@@ -0,0 +1,3 @@
+export let plusHighButtons = document.querySelectorAll (".new.high.task");
+export let plusLowButtons = document.querySelectorAll (".new.low.task");
+export let exButtons = document.querySelectorAll (".ex");
